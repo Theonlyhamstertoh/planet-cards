76b5394b8d95fdca74650ae4fd3c0a745008c22e
Theonlyhamstertoh <theonlyhamstertoh@gmail.com>
2021-06-01 17:36:06 -0700

    completed most of home design. still need to work on mobile responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    background: #000000;
+    padding: 0;
+    margin: 0;
+    width: 100vw;
+    min-height: 100vh;
+    font-family: "Open sans", sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+
+
+`;
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
